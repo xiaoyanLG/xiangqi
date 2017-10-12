6461002f00a7096c866557276fd0db182d394eeb
LUOGANG <luogang@acoinfo.com>
2017-10-12 15:09:01 +0800

    增加展示图片！

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 中国象棋
+
+![](https://github.com/xiaoyanLG/xiangqi/tree/master/ShowPictures/1.png)
+
+![](https://github.com/xiaoyanLG/xiangqi/tree/master/ShowPictures/2.png)
--- xybattleinfowidget.cpp
+++ xybattleinfowidget.cpp
@@ -10,13 +10,13 @@ XYBattleInfoWidget::XYBattleInfoWidget(QWidget *parent)
 {
     QVBoxLayout *layout = new QVBoxLayout(this);
     layout->addStretch(1);
-    QPushButton *closeBtn = new QPushButton("Close");
+    QPushButton *closeBtn = new QPushButton(QString::fromStdWString(L"退出"));
     connect(closeBtn, SIGNAL(clicked()), this, SLOT(exit()));
-    QPushButton *ZoomInBtn = new QPushButton("Zoom In");
+    QPushButton *ZoomInBtn = new QPushButton(QString::fromStdWString(L"放大"));
     connect(ZoomInBtn, SIGNAL(clicked()), this, SLOT(zoomIn()));
-    QPushButton *ZoomOutBtn = new QPushButton("Zoom Out");
+    QPushButton *ZoomOutBtn = new QPushButton(QString::fromStdWString(L"缩小"));
     connect(ZoomOutBtn, SIGNAL(clicked()), this, SLOT(zoomOut()));
-    QPushButton *layoutBtn = new QPushButton("Layout Qizi");
+    QPushButton *layoutBtn = new QPushButton(QString::fromStdWString(L"布棋"));
     connect(layoutBtn, SIGNAL(clicked()), this, SLOT(layoutQizi()));
 
     layout->addWidget(layoutBtn);
@@ -38,8 +38,8 @@ void XYBattleInfoWidget::paintEvent(QPaintEvent *event)
 void XYBattleInfoWidget::exit()
 {
     if (QMessageBox::question(this->parentWidget(),
-                              "Warning",
-                              "Are you sure to quit?",
+                              QString::fromStdWString(L"警告"),
+                              QString::fromStdWString(L"你确定退出吗?"),
                               QMessageBox::Yes|QMessageBox::No)
             == QMessageBox::Yes)
     {
